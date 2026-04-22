c721ce327c47f95371a620aff1330f7e98d167a3
hisu168 <dinhduchieu1608@gmail.com>
2026-04-22 22:06:30 +0700

    Leetcode 2452: Words Within Two Edits of Dictionary

---
+++ Leetcode/Problems/2452.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        vector<string> ans;
+
+        for(auto &s : queries) {
+            for(auto &t : dictionary) {
+                int cnt = 0;
+                for(int i = 0; i < s.size(); i++) {
+                    cnt += s[i] != t[i];
+                }
+                if(cnt < 3) {
+                    ans.push_back(s);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
